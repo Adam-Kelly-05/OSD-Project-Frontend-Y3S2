ca19970adec04f9cf14e56fa3503d0e5bd4721ce
Adam Kelly <adamkellyballintogher@gmail.com>
2026-02-26 00:54:29 +0000

    Added admin (untested)

--- src/app/admin/admin.ts
+++ src/app/admin/admin.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ListingService } from '../listings/listing.service';
+import { Observable } from 'rxjs';
+import { Listing } from '../listings/listing.interface';
+import words from 'profane-words';
+import { AsyncPipe } from '@angular/common';
+import { MatCard, MatCardTitle, MatCardContent } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [AsyncPipe, RouterLink, MatCard, MatCardTitle, MatCardContent],
   templateUrl: './admin.html',
   styleUrl: './admin.scss',
 })
-export class Admin {}
+export class Admin {
+  protected dataService = inject(ListingService);
+  listings$: Observable<Listing[]> = this.dataService.getListings();
+
+  protected containsProfanity(string: string): boolean {
+    const lowerString = string.toLowerCase();
+    return words.some((word) => lowerString.includes(word));
+  }
+}
